a4c6749e9bf0424fd21e8216ae88964f2d80bd31
tambarie <decagon@Decagons-MacBook-Air.local>
2021-07-26 21:56:11 +0100

    assignment one done

--- hello.go
+++ hello.go
@@ -3,5 +3,11 @@ package main
 import "fmt"
 
 func main (){
-	fmt.Println("hello world")
+	var name string = "Tambarie"
+	var stack string = "golang"
+	var squad string = "SQ008"
+	var year int64 = 2021
+
+
+	fmt.Println("My name is ",name,",I am a", stack ,"developer a member of squad", squad, "of the year ",year,".")
 }
